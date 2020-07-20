0f12a53779abff6243fede709cdd6c06a3e9de11
Vinicius <vinicius@iMac-de-Vinicius.local>
2020-07-20 15:21:14 -0300

    add

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Teste</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
